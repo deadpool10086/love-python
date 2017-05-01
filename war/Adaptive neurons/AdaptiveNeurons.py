c8ced1d353c9859ccfc5ae91ea5fac6057f4507a
deadpool10086 <zds19971227@126.com>
2017-05-01 15:36:33 +0800

    自适应神经元

--- war/Adaptive neurons/AdaptiveNeurons.py
+++ war/Adaptive neurons/AdaptiveNeurons.py
@@ -2,60 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-class Perceptron(object):
+class AdalineGD(object):
     """
-    eta : 学习率
-    n_iter:权重向量训练次数
-    w_: 神经分叉权重向量
-    errors:用于记录神经元判断出错的次数
+    eta:flaot
+    学习效率，处于0和1
 
+    n_iter:int
+    对训练数据进行学习改进次数
+
+    w_:一维向量
+    存储权重数值
+
+    error_:
+    存储器每次迭代是，网络对数据进行错误判断的次数
     """
-
-    def __init__(self,eta = 0.1, n_iter=10):
+    def __init__(self, eta=0.01, n_iter=50):
         self.eta = eta
         self.n_iter = n_iter
-        pass
-    def fit(self,X,y):
+    def fit(self, X, y):
         """
-        输入训练数据,培训神经元，x输入样本向量，y对应样本分类
-        X:shape[n_samples, n_features]
-        x:[[1,2,3],[4,5,6]]
-        n_samples: 2
-        n_features: 3
-        y:[1,-1]
-        """
+        X:二维数组[n_sampls, n_features]
+        n_samples 表示X中含有训练数据条目数
+        n_feature 含有4个数据的以为向量，用于表示一条训练条目
 
-        """
-        初始换权重向量为0
-        加一是因为前面算法提到的w0,也就是阈值
+        y:一维向量
+        用于存储每一训练条目对应的正确分类
+
         """
         self.w_ = np.zeros(1 + X.shape[1])
-        self.errors_ = []
-
-        for _ in range(self.n_iter):
-            errors = 0
-            """
-            X:[[1,2,3],[4,5,6]]
-            y:[1,-1]
-            zip[X,y] = [[1,2,3, 1],[4,5,6, -1]]
-            """
-            for xi, target in zip(X,y):
-                """
-                update = η * (y -y')
-                """
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
-                self.errors_.append(errors)
+        self.cost_ = []
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            #output = w0 + w1*x1 + w2*x2 .... wn*xn
+            print(output)
+            input()
+            errors = (y - output)
+            print(errors)
+            input()
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            print(self.w_[1:])
+            input()
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
     def net_input(self, X):
-        """
-        z = w0*l+w1*x1 +...wn*xn
-        """
         return np.dot(X,self.w_[1:]) + self.w_[0]
-
+    def activation(self, X):
+        return self.net_input(X)
     def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0 , 1, -1)
+        return np.where(self.activation(X) >= 0, 1, -1)
+
+
 df = pd.read_csv("D:\pywork\war\data.csv", header=None)
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa',-1,1)
@@ -67,13 +65,6 @@ plt.ylabel('花径的长度')
 plt.legend(loc = 'upper left')
 plt.show()
 
-ppn = Perceptron(eta=0.1, n_iter = 10)
-ppn.fit(X,y)
-
-plt.plot(range(1,len(ppn.errors_)+1),ppn.errors_, marker = 'o')
-plt.xlabel('Epochs')
-plt.ylabel('错误分类次数')
-plt.show()
 from matplotlib.colors import ListedColormap
 def plot_decision_regions(X, y, classifier, resolution = 0.02):
     markers = ('s','x','o','v')
@@ -92,8 +83,18 @@ def plot_decision_regions(X, y, classifier, resolution = 0.02):
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y==cl,0], y=X[y==cl,1], alpha=0.8, c=cmap(idx),marker=markers[idx], label = cl)
 
-plot_decision_regions(X, y, ppn, resolution = 0.02)
-plt.xlabel('花径长度')
+
+
+ada = AdalineGD(eta = 0.0001, n_iter=50)
+ada.fit(X,y)
+plot_decision_regions(X,y,classifier = ada)
+plt.title('Adaline-Gradient descent')
+plt.xlabel('花茎长度')
 plt.ylabel('花瓣长度')
-plt.legend(loc="upper left")
+plt.legend(loc = 'upper left')
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('sum-squard-error')
 plt.show()
