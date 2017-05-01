bdb96964944d67a2fa72e2905106118bbed0982e
deadpool10086 <zds19971227@126.com>
2017-05-01 12:21:39 +0800

    分类器

--- war/myKNN.py
+++ war/myKNN.py
@@ -1,4 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+myfont = fm.FontProperties(fname='C:/Windows/Fonts/msyh.ttc')
+import numpy as np
+import pandas as pd
 class Perceptron(object):
     """
     eta : 学习率
@@ -26,7 +31,7 @@ class Perceptron(object):
         初始换权重向量为0
         加一是因为前面算法提到的w0,也就是阈值
         """
-        self.w_ = np.zero(1 + X.shape[1])
+        self.w_ = np.zeros(1 + X.shape[1])
         self.errors_ = []
 
         for _ in range(self.n_iter):
@@ -45,11 +50,51 @@ class Perceptron(object):
                 self.w_[0] += update
                 errors += int(update != 0.0)
                 self.errors_.append(errors)
-    def net_input(self, x):
+    def net_input(self, X):
         """
-        z = w0*l w1*w1 +
+        z = w0*l+w1*x1 +...wn*xn
         """
-        return np.self.w_[1:] + self.w_[0]
+        return np.dot(X,self.w_[1:]) + self.w_[0]
 
-    def predict(self, x):
-        return np.where(self.net_input(x) >= 0.0 , 1, -1)
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0 , 1, -1)
+df = pd.read_csv("D:\pywork\war\data.csv", header=None)
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa',-1,1)
+X = df.iloc[0:100,[0,2]].values
+# plt.scatter(X[:50,0], X[:50,1], color='red', marker='o',label='setosa')
+# plt.scatter(X[50:100,0], X[50:100,1], color='blue', marker='x',label='versicolor')
+# plt.xlabel('花瓣的长度')
+# plt.ylabel('花径的长度')
+# plt.legend(loc = 'upper left')
+#plt.show()
+
+ppn = Perceptron(eta=0.1, n_iter = 10)
+ppn.fit(X,y)
+# plt.plot(range(1,len(ppn.errors_)+1),ppn.errors_, marker = 'o')
+# plt.xlabel('Epochs')
+# plt.ylabel('错误分类次数')
+# plt.show()
+from matplotlib.colors import ListedColormap
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    markers = ('s','x','o','v')
+    colors = ('red','blue', 'Lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() -1,X[:,0].max()
+    x2_min, x2_max = X[:, 1].min() -1,X[:,1].max()
+    xx1, xx2 = np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
+    z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.contour(xx1,xx2,z,alpha=0.4,cmap=cmap)
+    plt.xlim(xx1.min(),xx1.max())
+    plt.ylim(xx2.min(),xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y==cl,0], y=X[y==cl,1], alpha=0.8, c=cmap(idx),marker=markers[idx], label = cl)
+
+plot_decision_regions(X, y, ppn, resolution = 0.02)
+plt.xlabel('花径长度')
+plt.ylabel('花瓣长度')
+plt.legend(loc="upper left")
+plt.show()
---
+++ war/plt.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+myfont = fm.FontProperties(fname='C:/Windows/Fonts/msyh.ttc')
+import numpy as np
+import pandas as pd
+df = pd.read_csv("D:\pywork\war\data.csv", header=None)
+y = df.loc[0:100, 4].values
+y = np.where(y == 'Iris-setosa',-1,1)
+X = df.iloc[0:100,[0,2]].values
+plt.scatter(X[:50,0], X[:50,1], color='red', marker='o',label='setosa')
+plt.scatter(X[50:100,0], X[50:100,1], color='blue', marker='x',label='versicolor')
+plt.xlabel('花瓣的长度')
+plt.ylabel('花径的长度')
+plt.legend(loc = 'upper left')
+plt.show()
+from matplotlib.colors import ListedColormap
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    marker = ('s','x','o','v')
+    colors = ('red','blue', 'Lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() -1,X[:,0].max()
+    x2_min, x2_max = X[:, 1].min() -1,X[:,1].max()
+    print(x1_min, x1_max)
+    print(x2_min, x2_max)
+#plot_decision_regions(X, y, ppn, resolution = 0.02)
