3d4171ffcca1cfd47d218ec19662a32f833ec370
deadpool10086 <zds19971227@126.com>
2017-05-20 18:08:40 +0800

    朴素贝叶斯0.1

---
+++ war/Bayes/bayes.py
@@ -0,0 +1,42 @@
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', \
+    'problems', 'help', 'please'],
+    ['maybe', 'not', 'take', 'him', \
+    'to', 'dog', 'park', 'stupid'],
+    ['my', 'dalmation', 'is', 'so', 'cute', \
+    'I', 'love', 'him'],
+    ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+    ['mr', 'licks', 'ate', 'my', 'steak', 'how', \
+    'to', 'stop', 'him'],
+    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]
+    return postingList, classVec
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else: print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory)/float(numTrainDocs)
+    p0Num = zeros(numWords); plNum = zeros(numWords)
+    poDenom = 0.0; p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = p1Num/p1Denom
+    p0vect = p0num/p0Denom
+    return p0Vect, p1Vect, pAbusive
